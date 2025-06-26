91e9a3f87287b0416f6c226f28ef4120373d55bc
NixinityFeralRF <nixinityferalrf@gmail.com>
2025-06-26 19:34:49 +0300

    RSA working version.py

--- things.py
+++ things.py
@@ -11,27 +11,30 @@ q = int(input())
 n = p * q
 f = (p-1)*(q-1)
 
-e = int(input('простое число, меньше f и простое относительно f: '))
+e = int(input(f'Введите простое число, меньше {f} и простое относительно {f}: '))
 
-possible_d = []
-for d in range(1, 100):
-  if (d*e) % f == 1:
-    possible_d.append(d)
+possible_d = [0,]
+for D in range(1, n):
+  if (D*e) % f == 1:
+    possible_d.append(D)
     
 print ('как d можно выбрать одно из этих чисел:', possible_d)
+d = possible_d[-1]
 
-print ('Открытый ключ:', e, n)
-print ('Секретный ключ:', d, n)
+print ('Открытый ключ (e,n): ', e, n)
+print ('Секретный ключ (d,n): ', d, n)
 
 # 2
 
-encr_mess = (p ** e % n) # это Е
+M = int(input('Введите сообщение для кодировки: '))
+encr_mess = ((M ** e) % n) # это Е
 
 print('Закодированное сообщение, которое можно отправить: ', encr_mess)
 
 # 3
 
 E = encr_mess
-decr_mess = E ** d % n
+decr_mess = ((E ** d) % n)
 print('Раскодированное полученное сообщение: ', decr_mess)
 
+# извиняюсь если мой код непонятный
